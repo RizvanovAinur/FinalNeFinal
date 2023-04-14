5bab3e6b51d6d1f18aeba84fec33df53400be6a1
Ainur <rizvanov_ainur@mail.ru>
2023-04-14 11:21:02 +0300

    Начало проекта

---
+++ src/main/java/com/example/finalnefinal/FinalNeFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalnefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalNeFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalNeFinalApplication.class, args);
+    }
+
+}
